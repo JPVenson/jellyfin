a6c15477ee56d78d0c8062d3dccadf3c00c2d71a
JPVenson <github@jpb.software>
2025-03-22 20:52:51 +0000

    Updated copyright note

--- MediaBrowser.MediaEncoding/Properties/AssemblyInfo.cs
+++ MediaBrowser.MediaEncoding/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jellyfin Project")]
 [assembly: AssemblyProduct("Jellyfin Server")]
-[assembly: AssemblyCopyright("Copyright ©  2019 Jellyfin Contributors. Code released under the GNU General Public License")]
+[assembly: AssemblyCopyright("Copyright ©  2025 Jellyfin Contributors. Code released under the GNU General Public License")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
